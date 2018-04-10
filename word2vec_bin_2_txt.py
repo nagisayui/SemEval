389abc813dc26c7bd3bf6260900fe0f79adeaa2c
1 <1046574356@qq.com>
2018-04-10 23:36:05 +0800

    V0.5

--- word2vec_bin_2_txt.py
+++ word2vec_bin_2_txt.py
@@ -18,10 +18,34 @@ def txt2bin(model_path):
     fvocab = 'vocab.txt'
     word2vec_model.wv.save_word2vec_format(fname=bin_path, fvocab=fvocab, binary=True, total_vec=None)
 
+
+#计算行数，就是单词数
+def getFileLineNums(filename):
+    f = open(filename, 'r',encoding='utf-8')
+    count = 0
+    for line in f:
+        count += 1
+    return count
+
+# 在glove模型文件前增加一行
+def glove2wordvec(file_path,dim):
+    voc_size=getFileLineNums(file_path)
+    line='%d %d' % (voc_size,dim)
+    with open(file_path, 'r',encoding='utf-8') as fin:
+        with open(file_path+'.txt', 'w',encoding='utf-8') as fout:
+            fout.write(line + "\n")
+            for line in fin:
+                fout.write(line)
+
+
 if __name__=='__main__':
-    word2vec_model_path = os.path.join(RootPath, 'GoogleNews-vectors-negative300.bin')
-    # 从binary信息转为txt信息
-    bin2txt(word2vec_model_path)
+    # word2vec_model_path = os.path.join(RootPath, 'GoogleNews-vectors-negative300.bin')
+    # # 从binary信息转为txt信息
+    # bin2txt(word2vec_model_path)
+
     # 从txt信息转为bin信息
     # txt2bin(otherpath) #自定义路径
+
+    glove_path=os.path.join(RootPath,'glove.6B.300d.txt')
+    glove2wordvec(glove_path,300)
 
